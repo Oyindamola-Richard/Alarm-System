a4023cef93300937aa614b15ad6ce3475feda578
oyindamola richard <oyediranrichardoyindamola@gmail.com>
2022-08-25 15:53:44 +0100

    first commit

---
+++ alarm.js
@@ -0,0 +1,46 @@
+const song = new Audio('crazy-chicken.mp3');
+
+const timeDisp = ()=>{
+    const time = new Date();
+    
+    const hour = timeFormat((time.getHours()))
+    const minutes = timeFormat((time.getMinutes()))
+    const seconds = timeFormat((time.getSeconds()))
+    clock.innerHTML = `${hour} : ${minutes} : ${seconds}`
+}
+setInterval(timeDisp, 1000);
+
+const timeFormat = (timer)=>{
+    if(timer<10){
+    return '0'+timer
+    }
+    else{
+        return timer
+    }
+}
+
+
+const setAlarm = ()=>{
+    let alarmHrs = hours.value;
+    let alarmMin = minutes.value;
+    alarmTime = new Date();
+    
+    if(alarmHrs == alarmTime.getHours() && alarmMin == alarmTime.getMinutes()){
+        // alert("wake up")
+        song.play()
+    }
+    setInterval("setAlarm()", 1000);
+}
+
+const hey = ()=>{
+    alert('Answer this Question')
+    var wake = prompt('DO YOU HAVE CLASS TODAY?','yes or no')
+    if(wake == "no"){
+        alert('REST ON BRUV')
+        location.reload() 
+    }
+    else if (wake == "yes"){
+        alert('WAKE UP ALAYE')
+    }
+    
+}
